990513b1d3e57f74209f8f19dc8f47df75a0c902
allenaxie <allenaxie1@gmail.com>
2022-10-09 17:22:29 -0700

    Create layout for edit container.

--- pages/templates/productDetails/index.js
+++ pages/templates/productDetails/index.js
@@ -1,7 +1,7 @@
 import React, { useRef, useCallback } from 'react';
 import { toJpeg } from 'html-to-image';
 import classes from './productDetails.module.scss';
-import { Divider, Button } from 'antd';
+import { Divider, Button, Row, Col } from 'antd';
 import { ModelsInfo } from '../../../components';
 
 const productDetails = () => {
@@ -13,7 +13,7 @@ const productDetails = () => {
       return
     }
 
-    toJpeg(ref.current, { width: 400, canvasWidth:400 })
+    toJpeg(ref.current, { width: 400, canvasWidth: 400 })
       .then((dataUrl) => {
         const link = document.createElement('a')
         link.download = 'my-image-name.jpeg'
@@ -26,25 +26,32 @@ const productDetails = () => {
   }, [ref])
 
   return (
-    <>
-      <div ref={ref} className={classes.container}>
+    <Row className={classes.container}>
+      <Col xs={{ span: 11 }}>
         {/* DOM nodes you want to convert to JPEG */}
-        <div>
-          <div className={classes.productName_title}>
-            <span>twist top</span>
+        <div ref={ref} className={classes.contentContainer}>
+          <div>
+            <div className={classes.productName_title}>
+              <span>twist top</span>
+            </div>
+            <div className={classes.productName_description}>
+              <p>哈喽， 大家好。我是霸王。是王中之霸，不是王八蛋。如果有兴趣做朋友，请联系我的助手。</p>
+            </div>
           </div>
-          <div className={classes.productName_description}>
-            <p>哈喽， 大家好。我是霸王。是王中之霸，不是王八蛋。如果有兴趣做朋友，请联系我的助手。</p>
-          </div>
+          <Divider />
+          <ModelsInfo />
         </div>
-        <Divider/>
-    <ModelsInfo/>
-      </div>
-      <div className={classes.buttonContainer}>
-        <Button onClick={onButtonClick}>Click me</Button>
-      </div>
+        <div className={classes.buttonContainer}>
+          <Button onClick={onButtonClick}>Click me</Button>
+        </div>
+      </Col>
+      <Col xs={{ span: 11 }} className={classes.formContainer}>
+        <div style={{ position: 'fixed' }}>
+          <span>Edit form</span>
+        </div>
+      </Col>
+    </Row>
 
-    </>
   )
 }
 
